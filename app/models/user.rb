76ca0a1626c609e1e1709d96e2eed084bb3d12e5
milton2322 <produccion17@softpymes.com.co>
2017-02-23 17:39:10 -0500

    commit sucessfull

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
   has_many :articles
   has_many :comments
   include PermissionsConcern
+  
+  def avatar
+    email_address = "miltonjoseq@gmail.com".downcase
+ 
+    # create the md5 hash
+    hash = Digest::MD5.hexdigest(email_address)
+
+    # compile URL which can be used in <img src="RIGHT_HERE"...
+    image_src = "https://www.gravatar.com/avatar/#{hash}"
+  end
 end
